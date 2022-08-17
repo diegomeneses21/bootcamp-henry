820b0b61fba8d054ce2a79ac32038a92580b8ae4
diegomeneses21 <69326013+diegomeneses21@users.noreply.github.com>
2022-08-17 08:29:59 -0500

    ft01

--- FT-M1-master/01-IntroToCS/homework/homework.js
+++ FT-M1-master/01-IntroToCS/homework/homework.js
@@ -1,6 +1,6 @@
 'use strict'
 
-function BinarioADecimal(num) {
+function BinarioADecimal(string) {
   // tu codigo aca
 
   // let arr = []; 
@@ -14,13 +14,24 @@ function BinarioADecimal(num) {
 
   // return parseInt(num,2); 
 
-  var decimal = 0;
-  num = num.toString();
-  for (let i = 0; i < num.length; i++) {
-    console.log(num[i]);
-    decimal = decimal + num[i] * 2 ** (num.length - 1 - i)
+  // var decimal = 0;
+  // num = num.toString();
+  // for (let i = 0; i < num.length; i++) {
+  //   console.log(num[i]);
+  //   decimal = decimal + num[i] * 2 ** (num.length - 1 - i)
+  // }
+  // return decimal;
+
+  var result = 0; 
+  var count = string.length - 1;
+  for (let i = 0; i < string.length; i++) {
+    result = result + string[i] * Math.pow(2, count);
+    // var lugarPotencia =string.length - 1 
+    // console.log("count is: ", count)
+    count--
   }
-  return decimal;
+  // console.log("result is: ", result)
+  return result;
 }
 // console.log(BinarioADecimal(1011));
 
@@ -28,14 +39,22 @@ function BinarioADecimal(num) {
 function DecimalABinario(num) {
   // tu codigo aca
 
-  var n = num;
-  var bin = (n % 2).toString();
-  while (n > 1) {
-    n = parseInt(n / 2);
-    bin = (n % 2) + bin;
-  }
-  return bin;
+  // var n = num;
+  // var bin = (n % 2).toString();
+  // while (n > 1) {
+  //   n = parseInt(n / 2);
+  //   bin = (n % 2) + bin;
+  // }
+  // return bin;
 
+  var result = "";
+  var numero = num
+  while (numero>0) {
+    // console.log("numero es",numero)
+    result = numero%2 +result
+    numero = Math.floor(numero/2)
+  }
+  return result
 }
 // console.log(DecimalABinario(255))
 
